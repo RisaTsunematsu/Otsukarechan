2cc61d6306cbadc4e5be0b79ac5ac408e6b7bcb0
Risa Tsunematsu <purachan1818@gmail.com>
2023-05-22 06:50:06 +0000

    javaccript

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
+let promptStr = prompt('何か好きな文字を入力してください。');
 
-
-// メイン部分
-let alertString;
-
-// 作成した関数を呼び出し、変数へ格納
-alertString = addString("WebCamp");
-
-//変数の中身をアラートで表示する
-alert(alertString);
-
-// 作成した関数
-function addString(strA){
-  let addStr = "Hello " + strA;
-  return addStr;
-}
+alert(promptStr);
